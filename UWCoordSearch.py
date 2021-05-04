4b142c20c4e40bad6bbf3a08a43f919da7579034
avapolzin <ava.polzin@yale.edu>
2021-05-04 16:14:28 -0400

    Update repository path

--- UWCoordSearch.py
+++ UWCoordSearch.py
@@ -1,18 +1,28 @@
 import pandas as pd
+from astropy.coordinates import SkyCoord
 import datetime
 import sys
 from github import Github
 
 search = str(sys.argv[1])
 
-UWobs = pd.read_csv('https://raw.githubusercontent.com/avapolzin/UWStatusSearch/main/UWobs.txt', sep = '\t')
+UWobs = pd.read_csv('https://raw.githubusercontent.com/avapolzin/UWSStatusSearch/main/UWobs.txt', sep = '\t')
 
-result = UWobs.loc[UWobs.ID == search]
-if len(result) == 0:
-	raise Exception('Not a valid UW field name. Field IDs are listed UWXXX.')
+coords = SkyCoord(UWobs.RA, UWobs.Dec, unit = ['hr', 'deg'])
+
+c = SkyCoord(search, unit = ['hr', 'deg'])
+
+result = UWobs.loc[(abs(coords.ra.deg - c.ra.deg) <= 1.5) & (abs(coords.dec.deg - c.dec.deg) <= 0.9)]
+if len(result) ==0:
+	raise Exception('Search falls outside of the UWS footprint.')
+
 field = result.ID.values[0]
 Nframes = result.Nobs.values[0]
 obs_dates = result.obs_dates.values[0]
+
+print('\n\n')
+print('%s is located in %s'%(search, field))
+print('\n')
 
 if Nframes >= 100:
 	status = 'been fully observed'
@@ -22,12 +32,12 @@ if (Nframes < 100) and (Nframes > 0):
 	status = 'been partially observed'
 
 g = Github()
-repo = g.get_repo('avapolzin/UWSCoordSearch')
+repo = g.get_repo('avapolzin/UWSStatusSearch')
 commits = repo.get_commits(path='UWobs.txt')
 
 last_update = commits[0].commit.committer.date.strftime('%b %d %Y')
 
-print('\n\n')
+
 print('Observation status, last updated %s:\n'%last_update)
 print('%s has %s, with %i frames. \n'%(field, status, Nframes))
 print('Nights observed: %s'%(obs_dates))
