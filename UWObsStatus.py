99a8790a728c1e9bce609aa84b1428b649935015
avapolzin <ava.polzin@yale.edu>
2021-05-26 01:29:28 -0400

    Add central coordinates to readout

--- UWObsStatus.py
+++ UWObsStatus.py
@@ -2,6 +2,7 @@ import pandas as pd
 import datetime
 import sys
 from github import Github
+from astropy.coordinates import SkyCoord
 
 search = str(sys.argv[1])
 
@@ -13,6 +14,7 @@ if len(result) == 0:
 field = result.ID.values[0]
 Nframes = result.Nobs.values[0]
 obs_dates = result.obs_dates.values[0]
+field_center = SkyCoord(result.RA, result.Dec, unit = ['hr', 'deg'])
 
 if Nframes >= 100:
 	status = 'been fully observed'
@@ -27,6 +29,8 @@ commits = repo.get_commits(path='UWobs.txt')
 
 last_update = commits[0].commit.committer.date.strftime('%b %d %Y')
 
+print('\n\n')
+print('%s is centered at %s (in deg: %s).'%(search, str(field_center.to_string('hmsdms')).strip('[]\'\''), str(field_center.to_string('decimal')).strip('[]\'\'')))
 print('\n\n')
 print('Observation status, last updated %s:\n'%last_update)
 print('%s has %s (%i frames). \n'%(field, status, Nframes))
